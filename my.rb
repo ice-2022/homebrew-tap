785c0858b14bf10954331204298301442fce2dab
xujianbin <sanxing42008@vip.qq.com>
2022-11-09 12:42:54 +0800

    my-1.0.0

---
+++ my.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class MyCli < Formula
+    desc ""
+    homepage ""
+    url "https://github.com/ice-2022/my-cli/releases/download/1.0.0/my-cli.tar.gz"
+    sha256 "89bea6064df0d5d106282fe62c4fb9c778e2bb77463fbdf3d6d9a8dd3833790d"
+    license ""
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      # system "./configure", *std_configure_args, "--disable-silent-rules"
+      bin.install "my"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test my-cli`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
